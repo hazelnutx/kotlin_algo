d53632585479d495b3de5ff12574ad216f249715
hzlnt <raulrusu46@gmail.com>
2020-03-26 11:03:25 +0200

    X

--- src/Kyu8/Area_or_perimeter.kt
+++ src/Kyu8/Area_or_perimeter.kt
@@ -4,4 +4,9 @@ fun main(args: Array<String>) {
     println(areaOrPerimeter(4, 4))
 }
 
+/**
+ *  If the length and the width are equal we return the area of a square
+ *  otherwise, we return the perimeter of a rectangle
+ */
+
 fun areaOrPerimeter(l:Int, w:Int):Int = if (l == w) l * w else (l + w ) * 2
